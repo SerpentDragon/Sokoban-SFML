69312ee4ffc40641f4d5f37dbfec583f470ce0a4
SerpentDragin <shelyganovdmitry@gmail.com>
2023-03-29 22:15:37 +0300

    Deleted unnecessery comments

--- GameFiles/settings.h
+++ GameFiles/settings.h
@@ -4,21 +4,6 @@
 
 using namespace sf;
 
-// // parameters
-// static int ScreenWidth = VideoMode::getDesktopMode().width;
-// static int ScreenHeight = VideoMode::getDesktopMode().height;
-// static int Width = 0.625 * ScreenWidth; // game window width
-// static int Height = 0.74075 * ScreenHeight; // game window height
-// static int size = Width / 24; // size of cells
-
-// //colors
-// static Color WHITE(255, 255, 255);
-// static Color GREEN(0, 128, 0);
-// static Color BLUE(66, 170, 255);
-// static Color DARK_BLUE(0, 39, 174);
-// static Color GREY(128, 128, 128);
-// static Color GOLD(255, 215, 0);
-
 // parameters
 extern int ScreenWidth; // screen dimensions
 extern int ScreenHeight;
