543a5848013c9e70545ff5792b0036275a3b9455
Serpent Dragon <shelyganovdmitry@gmail.com>
2024-01-03 14:47:57 +0300

    Added Sound Manager class to upload and play sounds

---
+++ GameFiles/SoundManager.cpp
@@ -0,0 +1,36 @@
+#include "SoundManager.hpp"
+
+SoundManager* SoundManager::manager_ = nullptr;
+
+SoundManager* SoundManager::getManager()
+{
+    if (manager_ == nullptr) manager_ = new SoundManager();
+
+    return manager_;
+}
+
+void SoundManager::playSound(const std::string& name)
+{
+    sf::Sound sound(sounds_[name]);
+    sound.play();
+}
+
+SoundManager::SoundManager()
+{
+    loadAllSoundsFromDirectory();
+}
+
+void SoundManager::loadAllSoundsFromDirectory()
+{
+    const std::string dir = "music/";
+    for(const auto& entry : fs::directory_iterator(dir))
+    {
+        if (fs::is_regular_file(entry))
+        {
+            std::string filename = entry.path().filename();
+            std::string sound_name = entry.path().stem();
+
+            sounds_[sound_name].loadFromFile(filename);
+        }
+    }
+}
---
+++ GameFiles/SoundManager.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <SFML/Audio.hpp>
+#include <unordered_map>
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
+class SoundManager
+{
+public:
+
+    static SoundManager* getManager();
+
+    void playSound(const std::string&);
+
+private:
+
+    SoundManager();
+
+    SoundManager(const SoundManager&) = delete;
+
+    SoundManager& operator=(const SoundManager&) = delete;
+
+    void loadAllSoundsFromDirectory();
+
+private:
+
+    static SoundManager* manager_;
+
+    std::unordered_map<std::string, sf::SoundBuffer> sounds_;
+};
