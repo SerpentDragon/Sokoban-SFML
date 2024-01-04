bdf2f800a3fe8a693f80695c39685f8872bb8715
Serpent Dragon <shelyganovdmitry@gmail.com>
2024-01-04 20:42:45 +0300

    Added FileIOManager class to control reading & writing data files

---
+++ GameFiles/FileIOManager.cpp
@@ -0,0 +1,77 @@
+#include "FileIOManager.hpp"
+
+FileIOManager::FileIOManager()
+    : coinsPath_("data/coins.ltx"),
+    recordsPath_("data/records.ltx")
+{
+}
+
+std::pair<int, int> FileIOManager::readDataFromFile()
+{
+    if (!fs::exists("data/")) 
+        fs::create_directory("data/");
+
+    int coins_;
+    int record_;    
+
+    std::fstream coinsFile(coinsPath_, ib::in | ib::binary);
+    std::fstream recordsFile(recordsPath_, ib::in | ib::binary);
+
+    if (coinsFile.is_open())
+    {
+        try
+        {
+            coinsFile >> coins_;
+
+            if (coins_ < 0) coins_ = 0;
+
+            coinsFile.close();
+        }
+        catch(const std::exception& ex)
+        {
+            coins_ = 0;
+        }
+    }
+    else coins_ = default_coins_;
+
+    if (recordsFile.is_open())
+    {
+        try
+        {
+            recordsFile >> record_;
+
+            if (record_ < 0 || record_ >= levelsMap.size())
+                record_ = 0;
+
+            recordsFile.close();
+        }
+        catch(const std::exception& ex)
+        {
+            record_ = 0;
+        }
+        
+    }
+    else record_ = 0;
+
+    return { coins_, record_ };
+}
+
+void FileIOManager::writeDataToFile(int coins_, int record_)
+{
+    std::fstream coinsFile(coinsPath_, ib::out | ib::binary);
+    std::fstream recordsFile(recordsPath_, ib::out | ib::binary);
+
+    if (coinsFile.is_open())
+    {
+        coinsFile << coins_;
+        coinsFile.close();
+    }
+    else std::cerr << "Can not write coins file!\n";
+
+    if (recordsFile.is_open())
+    {
+        recordsFile << record_;
+        recordsFile.close();
+    }
+    else std::cerr << "Can not write records file!\n";
+}
---
+++ GameFiles/FileIOManager.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <filesystem>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "map.h"
+
+using ib = std::ios_base;
+namespace fs = std::filesystem;
+
+class FileIOManager
+{
+public:
+
+    FileIOManager();
+
+    FileIOManager(const FileIOManager&) = delete;
+
+    FileIOManager(FileIOManager&&) noexcept = delete;
+
+    FileIOManager& operator=(const FileIOManager&) = delete;
+
+    FileIOManager& operator=(FileIOManager&&) noexcept = delete;
+
+    std::pair<int, int> readDataFromFile();
+
+    void writeDataToFile(int, int);
+
+private:
+
+    std::string coinsPath_;
+    std::string recordsPath_;
+
+    const int default_coins_ = 50;
+    const int default_record_ = 0;
+};
